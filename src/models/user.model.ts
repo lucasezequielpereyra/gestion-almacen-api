8834411d8ad9d90286414ffe385bd896f6cbc9c4
Lucas Pereyra <pereyral@jefatura.gob.ar>
2023-01-30 19:54:19 -0300

    add user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,38 @@
+import { Schema, model } from 'mongoose';
+import { IUser } from '../interfaces/user.interface';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true
+    }
+  },
+  {
+    timestamps: true,
+    versionKey: false
+  }
+);
+
+userSchema.pre('save', async function (next) {
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+const UserModel = model<IUser>('User', userSchema);
+
+export default UserModel;
