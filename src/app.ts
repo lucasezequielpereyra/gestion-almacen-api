4c4205cb9ba458d5306ffc23ce8251500f2f7be4
Lucas Pereyra <pereyral@jefatura.gob.ar>
2023-02-12 12:28:31 -0300

    add initial create roles

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,27 @@
-import express, { Application, Request, Response, NextFunction } from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
-import bodyParser from 'body-parser';
+import express, { Application, Request, Response, NextFunction } from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import { connect } from './config/mongo'
+import { createRoles } from './libs/initialSetup'
 
-const app: Application = express();
+const app: Application = express()
+
+/*    CONFIGS    */
+connect()
+createRoles()
 
 /*    MIDDLEWARES    */
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
-app.use(morgan('dev'));
-app.use(cors());
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.use(morgan('dev'))
+app.use(cors())
 
 /*    ERROR    */
 app.use((req: Request, res: Response, next: NextFunction) => {
-  res.status(500).json('Server error');
-  next();
-});
+  res.status(500).json('Server error')
+  next()
+})
 
-export default app;
+export default app
