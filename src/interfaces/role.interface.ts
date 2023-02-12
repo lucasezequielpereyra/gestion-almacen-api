d82bc666835e0cb37c68dd2495b45ea22b213862
Lucas Pereyra <pereyral@jefatura.gob.ar>
2023-02-12 11:59:28 -0300

    add role interface

---
+++ src/interfaces/role.interface.ts
@@ -0,0 +1,6 @@
+import { ObjectId } from 'mongoose'
+
+export interface IRole {
+  _id: ObjectId
+  name: string
+}
--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -1,12 +1,12 @@
-import { ObjectId } from 'mongoose';
+import { ObjectId } from 'mongoose'
 
 export interface IUser {
-  _id: ObjectId;
-  username: string;
-  password: string;
-  email: string;
-  roles: [ObjectId];
-  refreshToken: [string];
-  createdAt: Date;
-  updatedAt: Date;
+  _id: ObjectId
+  username: string
+  password: string
+  email: string
+  roles: [ObjectId]
+  refreshToken: [string]
+  createdAt: Date
+  updatedAt: Date
 }
