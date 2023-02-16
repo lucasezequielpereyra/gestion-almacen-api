323a05909f6d9c5eef7347b68d3fcf7e0ead2e0e
Lucas Pereyra <pereyral@jefatura.gob.ar>
2023-02-15 22:55:45 -0300

    update app

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import express, { Application, Request, Response, NextFunction } from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 import bodyParser from 'body-parser'
+import cookieParser from 'cookie-parser'
 import { connect } from './config/mongo'
 import { createRoles } from './libs/initialSetup'
 import { router as registerRouter } from './routes/register.route'
+import { router as authRouter } from './routes/auth.route'
 
 const app: Application = express()
 
@@ -14,13 +16,15 @@ createRoles()
 
 /*    MIDDLEWARES    */
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
 app.use(express.static('public'))
 app.use(morgan('dev'))
 app.use(cors())
 
 /*    ROUTES    */
 app.use('/auth/register', registerRouter)
+app.use('/auth/login', authRouter)
 
 /*    ERROR    */
 app.use((req: Request, res: Response, next: NextFunction) => {
