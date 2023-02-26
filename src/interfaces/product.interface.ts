e718f3504de780089c1d55a953b2ecdd06a49608
Lucas Pereyra <pereyral@jefatura.gob.ar>
2023-02-25 21:14:52 -0300

    add product interface

---
+++ src/interfaces/product.interface.ts
@@ -0,0 +1,13 @@
+import { ObjectId } from "mongoose"
+
+export interface IProduct {
+    _id: string
+    sku: number
+    name: string
+    category: ObjectId
+    description: string
+    EAN: string
+    price_cost: number
+    price_sale: number
+    stock: number
+}
