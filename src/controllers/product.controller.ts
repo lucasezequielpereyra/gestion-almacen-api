38e6d47eb4706fb3a8b01ba06f5f6ec3fc34bba9
Lucas Pereyra <pereyral@jefatura.gob.ar>
2023-03-14 19:55:24 -0300

    update getProductsByOrganization response

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,110 +1,144 @@
-import Product from '../models/product.model';
-import Category from '../models/category.model';
-import Organization from '../models/organization.model';
-import logger from '../config/logger';
-import { Request, Response } from 'express';
+import Product from '../models/product.model'
+import Category from '../models/category.model'
+import Organization from '../models/organization.model'
+import logger from '../config/logger'
+import { Request, Response } from 'express'
 
 const handleNewProduct = async (req: Request, res: Response) => {
-    const { sku, name, category, description, EAN, price_cost, price_sale, stock, organization } = req.body;
+  const {
+    sku,
+    name,
+    category,
+    description,
+    EAN,
+    price_cost,
+    price_sale,
+    stock,
+    organization
+  } = req.body
 
-    if (!sku) return res.status(400).json({ error: 'Missing sku' });
-    if (!name) return res.status(400).json({ error: 'Missing name' });
-    if (!category) return res.status(400).json({ error: 'Missing category' });
-    if (!description) return res.status(400).json({ error: 'Missing description' });
-    if (!EAN) return res.status(400).json({ error: 'Missing EAN' });
-    if (!price_cost) return res.status(400).json({ error: 'Missing price_cost' });
-    if (!organization) return res.status(400).json({ error: 'Missing organization' });
+  if (!sku) return res.status(400).json({ error: 'Missing sku' })
+  if (!name) return res.status(400).json({ error: 'Missing name' })
+  if (!category) return res.status(400).json({ error: 'Missing category' })
+  if (!description)
+    return res.status(400).json({ error: 'Missing description' })
+  if (!EAN) return res.status(400).json({ error: 'Missing EAN' })
+  if (!price_cost) return res.status(400).json({ error: 'Missing price_cost' })
+  if (!organization)
+    return res.status(400).json({ error: 'Missing organization' })
 
-    try {
-        const foundCategory = await Category.findOne({ name: category });
-        if (!foundCategory) return res.status(400).json({ error: 'Category not found' });
-        const newProduct = new Product({
-            sku: sku,
-            name: name,
-            category: foundCategory,
-            description: description,
-            EAN: EAN,
-            price_cost: price_cost,
-            price_sale: price_sale,
-            stock: stock,
-            organization: organization
-        });
+  try {
+    const foundCategory = await Category.findOne({ name: category })
+    if (!foundCategory)
+      return res.status(400).json({ error: 'Category not found' })
+    const newProduct = new Product({
+      sku: sku,
+      name: name,
+      category: foundCategory,
+      description: description,
+      EAN: EAN,
+      price_cost: price_cost,
+      price_sale: price_sale,
+      stock: stock,
+      organization: organization
+    })
 
-        const savedProduct = await newProduct.save();
+    const savedProduct = await newProduct.save()
 
-        return res.status(201).json(`Product ${savedProduct.name} created`);
-    } catch (err) {
-        logger.error.error(err);
-        res.status(500).json({ error: err });
-    }
+    return res.status(201).json(`Product ${savedProduct.name} created`)
+  } catch (err) {
+    logger.error.error(err)
+    res.status(500).json({ error: err })
+  }
 }
 
 const getProductsByOrganization = async (req: Request, res: Response) => {
-    const { organization } = req.body;
-    if (!organization) return res.status(400).json({ error: 'Missing organization' });
+  const { organization } = req.body
+  if (!organization)
+    return res.status(400).json({ error: 'Missing organization' })
 
-    try {
-        const foundOrganization = await Organization.findOne({ _id: organization });
-        if (!foundOrganization) return res.status(400).json({ error: 'Organization not found' });
+  try {
+    const foundOrganization = await Organization.findOne({ _id: organization })
+    if (!foundOrganization)
+      return res.status(400).json({ error: 'Organization not found' })
 
-        const foundProducts = await Product.find({ organization: foundOrganization });
-        if (!foundProducts) return res.status(400).json({ error: 'Products not found' });
+    const foundProducts = await Product.find({
+      organization: foundOrganization
+    })
+    if (!foundProducts)
+      return res.status(400).json({ error: 'Products not found' })
 
-        return res.status(200).json(foundProducts);
-    } catch (error) {
-        logger.error.error(error);
-        res.status(500).json({ error: error });
-    }
+    return res.status(200).json({ foundProducts })
+  } catch (error) {
+    logger.error.error(error)
+    res.status(500).json({ error: error })
+  }
 }
 
 const handleUpdateProduct = async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const { sku, name, category, description, EAN, price_cost, price_sale, stock, organization } = req.body;
+  const { id } = req.params
+  const {
+    sku,
+    name,
+    category,
+    description,
+    EAN,
+    price_cost,
+    price_sale,
+    stock,
+    organization
+  } = req.body
 
-    try {
-        const foundProduct = await Product.findById(id);
-        if (!foundProduct) return res.status(404).json({ error: 'Product not found' });
+  try {
+    const foundProduct = await Product.findById(id)
+    if (!foundProduct)
+      return res.status(404).json({ error: 'Product not found' })
 
-        const foundCategory = await Category.findOne({ name: category });
-        if (!foundCategory) return res.status(400).json({ error: 'Category not found' });
+    const foundCategory = await Category.findOne({ name: category })
+    if (!foundCategory)
+      return res.status(400).json({ error: 'Category not found' })
 
-        const updateProduct = await Product.findOneAndUpdate({ _id: foundProduct._id }, {
-            sku: sku || foundProduct.sku,
-            name: name || foundProduct.name,
-            category: foundCategory || foundProduct.category,
-            description: description || foundProduct.description,
-            EAN: EAN || foundProduct.EAN,
-            price_cost: price_cost || foundProduct.price_cost,
-            price_sale: price_sale || foundProduct.price_sale,
-            stock: stock || foundProduct.stock,
-            organization: organization
-        }, { new: true });
+    const updateProduct = await Product.findOneAndUpdate(
+      { _id: foundProduct._id },
+      {
+        sku: sku || foundProduct.sku,
+        name: name || foundProduct.name,
+        category: foundCategory || foundProduct.category,
+        description: description || foundProduct.description,
+        EAN: EAN || foundProduct.EAN,
+        price_cost: price_cost || foundProduct.price_cost,
+        price_sale: price_sale || foundProduct.price_sale,
+        stock: stock || foundProduct.stock,
+        organization: organization
+      },
+      { new: true }
+    )
 
-        return res.status(200).json(updateProduct);
-    } catch (error) {
-        logger.error.error(error);
-        res.status(500).json({ error: error });
-    }
+    return res.status(200).json(updateProduct)
+  } catch (error) {
+    logger.error.error(error)
+    res.status(500).json({ error: error })
+  }
 }
 
 const handleDeleteProduct = async (req: Request, res: Response) => {
-    const { id } = req.params;
+  const { id } = req.params
 
-    try {
-        const deleteProduct = await Product.findByIdAndDelete(id);
-        if (!deleteProduct) return res.status(404).json({ error: 'Product not found' });
+  try {
+    const deleteProduct = await Product.findByIdAndDelete(id)
+    if (!deleteProduct)
+      return res.status(404).json({ error: 'Product not found' })
 
-        return res.status(200).json(`Product ${deleteProduct.name} deleted`);
-    } catch (error) {
-        logger.error.error(error);
-        res.status(500).json({ error: error });
-    }
+    return res.status(200).json(`Product ${deleteProduct.name} deleted`)
+  } catch (error) {
+    logger.error.error(error)
+    res.status(500).json({ error: error })
+  }
 }
 
-
 export default {
-    handleNewProduct,
-    getProductsByOrganization,
-    handleUpdateProduct,
-    handleDeleteProduct
+  handleNewProduct,
+  getProductsByOrganization,
+  handleUpdateProduct,
+  handleDeleteProduct
 }
