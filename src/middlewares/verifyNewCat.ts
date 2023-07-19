a8d1f1eb3da5af05de89a776b445f7c5a3dfaac7
Lucas Pereyra <pereyral@jefatura.gob.ar>
2023-07-19 11:25:31 -0300

    uodate - add error msg in category middleware

--- src/middlewares/verifyNewCat.ts
+++ src/middlewares/verifyNewCat.ts
@@ -1,15 +1,18 @@
 import Category from '../models/category.model'
 import { Request, Response, NextFunction } from 'express'
 
-export const checkDuplicateCategories = async (req: Request, res: Response, next: NextFunction) => {
-    const { name } = req.body
+export const checkDuplicateCategories = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { name } = req.body
 
-    try {
-        const foundCategory = await Category.findOne({ name: name })
-        if (foundCategory) return res.sendStatus(400)
+  try {
+    const foundCategory = await Category.findOne({ name: name })
+    if (foundCategory)
+      return res.status(400).json({ error: 'Esta categoria ya existe' })
 
-        next()
-    } catch (error) {
-
-    }
+    next()
+  } catch (error) {}
 }
--- src/routes/api/product.route.ts
+++ src/routes/api/product.route.ts
@@ -1,11 +1,19 @@
-import { Router } from 'express';
-import producController from '../../controllers/product.controller';
-import { verifyEncargado, verifyEmpleado } from '../../middlewares/verifyJwt';
-import { checkDuplicateProducts } from '../../middlewares/verifyNewPrd';
+import { Router } from 'express'
+import producController from '../../controllers/product.controller'
+import { verifyEncargado, verifyEmpleado } from '../../middlewares/verifyJwt'
+import { checkDuplicateProducts } from '../../middlewares/verifyNewPrd'
 
-export const router = Router();
+export const router = Router()
 
-router.post('/', [verifyEncargado, checkDuplicateProducts], producController.handleNewProduct);
-router.get('/', [verifyEmpleado], producController.getProductsByOrganization);
-router.put('/:id', [verifyEncargado, checkDuplicateProducts], producController.handleUpdateProduct);
-router.delete('/:id', [verifyEncargado], producController.handleDeleteProduct);
+router.post(
+  '/',
+  [verifyEncargado, checkDuplicateProducts],
+  producController.handleNewProduct
+)
+router.get('/', [verifyEmpleado], producController.getProductsByOrganization)
+router.put(
+  '/:id',
+  [verifyEncargado, checkDuplicateProducts],
+  producController.handleUpdateProduct
+)
+router.delete('/:id', [verifyEncargado], producController.handleDeleteProduct)
