78faabc0f0050d89bf5b08bd5ffe23c1d4ed0427
Lucas Pereyra <pereyral@jefatura.gob.ar>
2023-08-04 00:36:24 -0300

    fix - middleware is not unique

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -2,46 +2,45 @@ import { Schema, model } from 'mongoose'
 import { IProduct } from '../interfaces/product.interface'
 
 const productSchema = new Schema({
-    sku: {
-        type: Number,
-        required: true,
-        unique: true
-    },
-    name: {
-        type: String,
-        required: true
-    },
-    category: {
-        ref: 'Category',
-        type: Schema.Types.ObjectId
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    EAN: {
-        type: String,
-        unique: true
-    },
-    price_cost: {
-        type: Number,
-        required: true
-    },
-    price_sale: {
-        type: Number,
-    },
-    stock: {
-        type: Number,
-    },
-    organization: {
-        ref: 'Organization',
-        type: Schema.Types.ObjectId,
-        required: true
-    },
-    deleted: {
-        type: Boolean,
-        default: false
-    }
+  sku: {
+    type: Number,
+    required: true,
+    unique: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  category: {
+    ref: 'Category',
+    type: Schema.Types.ObjectId
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  EAN: {
+    type: String
+  },
+  price_cost: {
+    type: Number,
+    required: true
+  },
+  price_sale: {
+    type: Number
+  },
+  stock: {
+    type: Number
+  },
+  organization: {
+    ref: 'Organization',
+    type: Schema.Types.ObjectId,
+    required: true
+  },
+  deleted: {
+    type: Boolean,
+    default: false
+  }
 })
 
 const ProductModel = model<IProduct>('Product', productSchema)
