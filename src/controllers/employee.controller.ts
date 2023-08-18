192bb69254f8c2b7046b1302b2e5bdd593ac4576
Lucas Pereyra <pereyral@jefatura.gob.ar>
2023-08-18 13:59:46 -0300

    fix - indentation

--- src/controllers/employee.controller.ts
+++ src/controllers/employee.controller.ts
@@ -7,93 +7,103 @@ import { IUser } from '../interfaces/user.interface'
 import bcrypt from 'bcryptjs'
 
 const handleNewEmployee = async (req: Request, res: Response) => {
-    const { employeeUser, password, email, employeeRoles, organization } = req.body
-    if (!employeeUser || !password || !email || !employeeRoles || !organization) return res.status(400).json({ message: 'Missing parameters' })
+  const { employeeUser, password, email, employeeRoles, organization } =
+    req.body
+  if (!employeeUser || !password || !email || !employeeRoles || !organization)
+    return res.status(400).json({ message: 'Missing parameters' })
 
-    try {
-        const foundRoles = await Role.find({ name: { $in: employeeRoles } })
+  try {
+    const foundRoles = await Role.find({ name: { $in: employeeRoles } })
 
-        console.log(employeeRoles)
+    const foundOrganization = await Organization.find({ name: organization })
 
-        const foundOrganization = await Organization.find({ name: organization })
+    const newUser = new User({
+      username: employeeUser,
+      password,
+      email,
+      roles: foundRoles.map(role => role._id),
+      organization: foundOrganization.map(organization => organization._id)
+    })
 
-        const newUser = new User({
-            username: employeeUser,
-            password,
-            email,
-            roles: foundRoles.map(role => role._id),
-            organization: foundOrganization.map(organization => organization._id)
-        })
+    await newUser.save()
 
-        await newUser.save()
-
-        return res.status(201).json({ message: 'User created', user: newUser })
-    } catch (error) {
-        logger.error.error(error)
-        return res.status(500).json({ message: 'Internal server error', error })
-    }
+    return res.status(201).json({ message: 'User created', user: newUser })
+  } catch (error) {
+    logger.error.error(error)
+    return res.status(500).json({ message: 'Internal server error', error })
+  }
 }
 
 const handleUpdateEmployee = async (req: Request, res: Response) => {
-    const { id } = req.params
-    const { employeeUser, password, email, employeeRoles, organization } = req.body
+  const { id } = req.params
+  const { employeeUser, password, email, employeeRoles, organization } =
+    req.body
 
-    if (!id) return res.status(400).json({ message: 'Missing parameters' })
+  if (!id) return res.status(400).json({ message: 'Missing parameters' })
 
-    try {
-        const foundEmployee: IUser | null = await User.findOne({ _id: id })
-        if (!foundEmployee) return res.status(404).json({ message: 'Employee not found' })
+  try {
+    const foundEmployee: IUser | null = await User.findOne({ _id: id })
+    if (!foundEmployee)
+      return res.status(404).json({ message: 'Employee not found' })
 
-        const foundRoles = await Role.find({ name: { $in: employeeRoles } })
+    const foundRoles = await Role.find({ name: { $in: employeeRoles } })
 
-        const foundOrganization = await Organization.find({ name: organization })
+    const foundOrganization = await Organization.find({ name: organization })
 
-        const passwordHash = await bcrypt.hash(password, 10)
+    const passwordHash = await bcrypt.hash(password, 10)
 
-        const updateEmployee = await User.findOneAndUpdate({ _id: foundEmployee._id }, {
-            username: employeeUser || foundEmployee.username,
-            password: passwordHash || foundEmployee.password,
-            email: email || foundEmployee.email,
-            roles: foundRoles.map(role => role._id) || foundEmployee.roles,
-            organization: foundOrganization.map(organization => organization._id) || foundEmployee.organization
-        }, { new: true })
+    const updateEmployee = await User.findOneAndUpdate(
+      { _id: foundEmployee._id },
+      {
+        username: employeeUser || foundEmployee.username,
+        password: passwordHash || foundEmployee.password,
+        email: email || foundEmployee.email,
+        roles: foundRoles.map(role => role._id) || foundEmployee.roles,
+        organization:
+          foundOrganization.map(organization => organization._id) ||
+          foundEmployee.organization
+      },
+      { new: true }
+    )
 
-        res.status(200).json({ message: 'Employee updated', employee: updateEmployee })
-
-    } catch (error) {
-        logger.error.error(error)
-        return res.status(500).json({ message: 'Internal server error', error })
-    }
+    res
+      .status(200)
+      .json({ message: 'Employee updated', employee: updateEmployee })
+  } catch (error) {
+    logger.error.error(error)
+    return res.status(500).json({ message: 'Internal server error', error })
+  }
 }
 
 const handleGetEmployees = async (req: Request, res: Response) => {
-    try {
-        const getRoles = await Role.find({
-            name: {
-                $in: ['dueño', 'empleado', 'encargado']
-            }
-        })
+  try {
+    const getRoles = await Role.find({
+      name: {
+        $in: ['dueño', 'empleado', 'encargado']
+      }
+    })
 
-        if (getRoles) {
-            const employees = await User.find({
-                roles: {
-                    $in: getRoles.map(role => role._id)
-                }
-            }).populate('roles').populate('organization')
-
-            res.status(200).json({ message: 'Employees found', employees })
-        } else {
-            res.status(404).json({ message: 'Employee role not found' })
+    if (getRoles) {
+      const employees = await User.find({
+        roles: {
+          $in: getRoles.map(role => role._id)
         }
+      })
+        .populate('roles')
+        .populate('organization')
 
-    } catch (error) {
-        logger.error.error(error)
-        return res.status(500).json({ message: 'Internal server error', error })
+      res.status(200).json({ message: 'Employees found', employees })
+    } else {
+      res.status(404).json({ message: 'Employee role not found' })
     }
+  } catch (error) {
+    logger.error.error(error)
+    return res.status(500).json({ message: 'Internal server error', error })
+  }
 }
 
 export default {
-    handleNewEmployee,
-    handleUpdateEmployee,
-    handleGetEmployees
+  handleNewEmployee,
+  handleUpdateEmployee,
+  handleGetEmployees
 }
