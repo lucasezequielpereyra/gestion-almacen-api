2cc5f1d700dd922936a95783994d3368f0e152e5
Lucas Pereyra <pereyral@jefatura.gob.ar>
2023-08-20 05:00:16 -0300

    feat - add newEmployee by organization endpoint

--- src/controllers/owner.controller.ts
+++ src/controllers/owner.controller.ts
@@ -1,5 +1,9 @@
 import Organization from '../models/organization.model'
 import { IOrganization } from '../interfaces/organization.interface'
+import User from '../models/user.model'
+import { IUser } from '../interfaces/user.interface'
+import Role from '../models/role.model'
+import { IRole } from '../interfaces/role.interface'
 import logger from '../config/logger'
 import { Request, Response } from 'express'
 
@@ -25,6 +29,52 @@ const getOrgazationEmployees = async (req: Request, res: Response) => {
   }
 }
 
+const handleNewEmployee = async (req: Request, res: Response) => {
+  const { organization, username, password, email, roles } = req.body
+  if (!organization)
+    return res.status(400).json({ error: 'Missing organization' })
+
+  if (!username) return res.status(400).json({ error: 'Missing username' })
+  if (!password) return res.status(400).json({ error: 'Missing password' })
+  if (!email) return res.status(400).json({ error: 'Missing email' })
+  if (!roles) return res.status(400).json({ error: 'Missing roles' })
+
+  try {
+    // get role
+    const dataRoles: IRole | null = await Role.findOne({ name: roles }).exec()
+    if (!dataRoles) return res.status(400).json({ error: 'Role not found' })
+
+    const newEmployee = new User({
+      username: username,
+      password: password,
+      email: email,
+      roles: dataRoles._id
+    })
+
+    const savedEmployee: IUser = await newEmployee.save()
+
+    // update org
+    const updatedOrganization: IOrganization | null =
+      await Organization.findOneAndUpdate(
+        {
+          _id: organization
+        },
+        {
+          $push: { employees: savedEmployee._id }
+        }
+      ).exec()
+
+    if (!updatedOrganization)
+      return res.status(400).json({ error: 'Organization not found' })
+
+    return res.status(201).json(`User ${savedEmployee.username} created`)
+  } catch (error) {
+    logger.error.error(error)
+    return res.status(500).json({ error: error })
+  }
+}
+
 export default {
-  getOrgazationEmployees
+  getOrgazationEmployees,
+  handleNewEmployee
 }
--- src/routes/api/owner.route.ts
+++ src/routes/api/owner.route.ts
@@ -1,6 +1,12 @@
 import { Router } from 'express'
 import ownerController from '../../controllers/owner.controller'
+import { checkDuplicateUsernameOrEmail } from '../../middlewares/verifySignup'
 
 export const router = Router()
 
 router.get('/employees', ownerController.getOrgazationEmployees)
+router.post(
+  '/employees',
+  [checkDuplicateUsernameOrEmail],
+  ownerController.handleNewEmployee
+)
