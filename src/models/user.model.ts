3762500d9b0b9a357da98dd4cb75e514f743b762
Lucas Pereyra <pereyral@jefatura.gob.ar>
2023-09-15 00:39:46 -0300

    update - username in user is unique

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -7,7 +7,8 @@ const userSchema = new Schema(
     username: {
       type: String,
       required: true,
-      trim: true
+      trim: true,
+      unique: true
     },
     password: {
       type: String,
